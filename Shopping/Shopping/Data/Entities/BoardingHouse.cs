f5d7e083506589fa6ee6fbe9376ca527fb841de7
ingeniero26 <ingjerson2014@gmail.com>
2024-06-28 07:34:09 -0500

    table-pension
    se agrega la tabla pensiones y ajustes en las vista interfaz de usuario

---
+++ Shopping/Shopping/Data/Entities/BoardingHouse.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class BoardingHouse
+    {
+        private int Id { get; set; }
+        [Display(Name = "Pensión")]
+        [MaxLength(50, ErrorMessage = ("El campo  debe tener un  {1} máximo de caracteres "))]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; }
+        [Display(Name = "Nit Pensión")]
+        [MaxLength(50, ErrorMessage = ("El campo  debe tener un  {1} máximo de caracteres "))]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string NitBoardingHouse { get; set; }
+        [Display(Name = "Dirección")]
+        public string Address { get; set;}
+        [Display(Name = "Teléfono")]
+        public string Phone { get; set; }
+        [Display(Name = "Correo")]
+        public string Email { get; set; }
+
+    }
+}
